655ff4768fcbb572356bc40d13325aa5bf6dd5ed
Ryo Kimura <rq5273@gmail.com>
2022-12-21 15:54:04 +0900

    modified

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
+echo "Cloning SoftEtherVPN_Stable sourcecode from the repository."
 git clone https://github.com/SoftEtherVPN/SoftEtherVPN_Stable.git
 cp Dockerfile ./SoftEtherVPN_Stable
+echo "Building the container."
 podman build -t vpnserver ./SoftEtherVPN_Stable/.
+echo "Running the container and bash will be shown."
+echo "Please enter \"vpnserver start\" and then \"vpncmd\" to configure settings."
 podman run -it --privileged --network host --restart always vpnserver
