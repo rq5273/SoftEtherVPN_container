fe3434af571d08e1c21c0024d179462412468824
Ryo Kimura <rq5273@gmail.com>
2022-12-21 18:05:48 +0900

    modified

--- install.sh
+++ install.sh
@@ -5,6 +5,5 @@ cp cmd.sh ./SoftEtherVPN_Stable
 echo "Building the container."
 podman build -t vpnserver ./SoftEtherVPN_Stable/.
 echo "Running the container and bash will be shown."
-echo "Please enter \"vpnserver start\" and then \"vpncmd\" to configure settings."
-echo "If you want to run \"vpnserver start\" in container automatically when your PC starts, please use cron."
+echo "Please enter \"vpncmd\" to configure settings."
 podman run -it --privileged --network host --restart always vpnserver
